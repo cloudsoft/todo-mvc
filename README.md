6078f83fdb2c1203f58ad6962e5c32eac2a4a9e0
Justin Thompson <justin.thompson@cloudsoftcorp.com>
2017-04-03 14:47:17 +0100

    - Initial commit of the Angular App - Initial commit of the Node JS Server

---
+++ README.md
@@ -0,0 +1,44 @@
+# Todo-mvc
+Simple TODO list app using Node.js, Express and MySQL with an angular front end.
+
+# Building
+`
+npm install
+`
+
+# Running
+Replace the environment variables with your own config 
+`
+DB_HOST=127.0.0.1 DB_PORT=3306 DB_USER=brooklyn DB_PASSWORD=br00k11n DB_NAME=todo node app.js
+`
+ 
+# App
+Once the app has started, you can access it at http://localhost:8000/app
+
+# Credits
+This front end of this application is based on https://github.com/tastejs/todomvc/tree/gh-pages/examples/angularjs
+The Node JS side is based on https://github.com/yogasaikrishna/todo-app
+
+# Licence
+
+MIT License
+
+Copyright (c) 2017 Cloudsoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var api = require('./server/api');
+var expressMetrics = require('express-metrics');
+
+var app = express();
+var pool;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+var error = function(msg) {
+  throw msg;
+};
+
+var initDatabaseConnection = function () {
+  var host = process.env.DB_HOST || error("Missing configuration: DB_HOST");
+  var port = process.env.DB_PORT || error("Missing configuration: DB_PORT");
+  var user = process.env.DB_USER || error("Missing configuration: DB_USER");
+  var password = process.env.DB_PASSWORD || error("Missing configuration: DB_PASSWORD");
+  var database = process.env.DB_NAME || error("Missing configuration: DB_NAME");
+
+  console.log("Connecting to " + user + "@" + host + ":" + port + "/" + database);
+  pool = mysql.createPool({
+    connectionLimit: 10,
+    host: host,
+    port: port,
+    user: user,
+    password: password,
+    database: database
+  });
+};
+
+app.mySqlConnection = function (callback) {
+  pool.getConnection(function (err, connection) {
+    callback(err, connection);
+  });
+}
+
+var configureMetrics = function () {
+  var metricsPort = process.env.METRICS_PORT || 8091;
+  app.use(expressMetrics({
+    port: metricsPort
+  }));
+  console.log('Configured express metrics on port ' + metricsPort);
+};
+
+var startServer = function () {
+  var serverPort = process.env.SERVER_PORT || 8000;
+  app.listen(serverPort, function () {
+    console.log('Started server on port ' + serverPort);
+  });
+};
+
+initDatabaseConnection();
+configureMetrics();
+api.configure(app);
+startServer();
+
+
